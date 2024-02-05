8caa9b94a5341be11560f1093b9ed8525567a2fd
coffe-machine <udrismugurel03@yahoo.com>
2024-02-05 21:03:20 +0200

    adding a small fix

--- snake.py
+++ snake.py
@@ -19,15 +19,12 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-
     def add_segment(self, position):
         new_segment = Turtle(shape='square')
         new_segment.color('white')
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
-
-
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
