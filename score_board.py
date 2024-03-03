8ea911641b40c0a6c03c9b41562b3211b0492081
coffe-machine <udrismugurel03@yahoo.com>
2024-03-03 19:45:34 +0200

    Adding the high_score with file writing

--- score_board.py
+++ score_board.py
@@ -12,12 +12,15 @@ class Score_board(Turtle):
         self.hideturtle()
         self.goto(0, 278)
         self.current_score = 0
-        center = 'center'
-        self.write("Score : " + str(self.current_score), True, align=ALIGN, font=FONT)
+        self.highest_score = self.read_highest_score()
+        self.write("Score : " + str(self.current_score) + " Highest Score: " + str(self.highest_score), True,
+                   align=ALIGN,
+                   font=FONT)
 
     def write_score(self):
         self.clear()
-        self.write("Score : " + str(self.current_score), True, align=ALIGN, font=FONT)
+        self.write("Score : " + str(self.current_score) + " Highest Score: " + str(self.highest_score), True,
+                   align=ALIGN, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
@@ -25,5 +28,16 @@ class Score_board(Turtle):
 
     def increase_score(self):
         self.current_score += 1
+        if self.current_score > self.highest_score:
+            self.highest_score = self.current_score
+            self.update_highest_score()
         self.goto(0, 278)
         self.write_score()
+
+    def read_highest_score(self):
+        with open("data.txt", "r") as file:
+            return int(file.read())
+
+    def update_highest_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.highest_score))
